332e0997aeeb3da7ebc12dffd6ad0d5da491fe36
LeandroFilie <leandro.gfilie@gmail.com>
2023-10-22 14:55:49 -0300

    chore: initial setup

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import { AddressInfo } from 'net';
+
+import cors from 'cors';
+import dotenv from 'dotenv';
+import express from 'express';
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello World' });
+});
+
+const server = app.listen(process.env.PORT || 3000, () => {
+  if (server) {
+    const address = server.address() as AddressInfo;
+    console.log(`Server running at http://localhost:${address.port}`);
+  } else {
+    console.error('Failure initializing server.');
+  }
+});
