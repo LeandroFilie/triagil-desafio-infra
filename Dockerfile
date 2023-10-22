ae3f29da4387abf80a20389de57e9253bef56d01
LeandroFilie <leandro.gfilie@gmail.com>
2023-10-22 20:53:28 -0300

    chore: added docker config

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM node:alpine
+
+WORKDIR /usr/src/triagilapp
+
+RUN apk update && apk upgrade
+RUN apk add git
+RUN apk add bash
+
+RUN rm -rf ./node_modules
+RUN rm -rf package-lock.json
+
+COPY ./package.json .
+RUN npm install
+
+COPY . .
+
+EXPOSE 3003
+
+CMD npm run build && npm run start
---
+++ src/data/script.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS triagil;
+CREATE DATABASE triagil;
+USE triagil;
+
+CREATE TABLE IF NOT EXISTS team (
+	id varchar(55) primary key,
+  owner varchar(50) not null
+) engine=InnoDB;
+
+CREATE TABLE IF NOT EXISTS pokemon (
+	id varchar(55) primary key,
+  nome varchar(40) not null,
+  height float not null,
+  weight float not null
+) engine=InnoDB;
+
+CREATE TABLE IF NOT EXISTS team_pokemon (
+	id varchar(55) primary key,
+	id_team varchar(55) not null,
+  id_pokemon varchar(55) not null,
+  FOREIGN KEY (id_team)
+    REFERENCES team (id)
+    ON UPDATE CASCADE ON DELETE CASCADE,
+	FOREIGN KEY (id_pokemon)
+    REFERENCES pokemon (id)
+    ON UPDATE CASCADE ON DELETE CASCADE
+) engine=InnoDB;
